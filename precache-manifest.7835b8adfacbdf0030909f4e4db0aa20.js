8904def5f0a0cbb78dd879fad3851673a18754c3
Ashutosh Sharma <ashutoshsharma9865@gmai9l.com>
2022-06-26 21:38:22 +0530

    Updates

---
+++ precache-manifest.7835b8adfacbdf0030909f4e4db0aa20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c88a7b00dac583df7704",
+    "url": "/RajeshSharma/static/css/main.ae37a7f6.chunk.css"
+  },
+  {
+    "revision": "c88a7b00dac583df7704",
+    "url": "/RajeshSharma/static/js/main.b7ddc3d5.chunk.js"
+  },
+  {
+    "revision": "e4548098d066d6b58403",
+    "url": "/RajeshSharma/static/js/runtime~main.6175d000.js"
+  },
+  {
+    "revision": "bcee61dffe864b1c9d49",
+    "url": "/RajeshSharma/static/js/2.2d10c16a.chunk.js"
+  },
+  {
+    "revision": "6c63b180cf603c870506",
+    "url": "/RajeshSharma/static/js/3.943ca5f7.chunk.js"
+  },
+  {
+    "revision": "34c57b13a113a7887ba7070b18a3318d",
+    "url": "/RajeshSharma/index.html"
+  }
+];
